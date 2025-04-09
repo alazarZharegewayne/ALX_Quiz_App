8864026bd2b9e0a398f77cfbba3da048d1024002
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-04-09 15:38:34 +0300

    Fixing untracked file errors

--- quiz-app/src/services/api.js
+++ quiz-app/src/services/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://opentdb.com/api.php';
+const API_URL = "https://opentdb.com/api.php";
 
 export const fetchCategories = async () => {
   try {
-    const response = await axios.get('https://opentdb.com/api_category.php');
+    const response = await axios.get("https://opentdb.com/api_category.php");
     return response.data.trivia_categories;
   } catch (error) {
-    console.error('Error fetching categories:', error);
+    console.error("Error fetching categories:", error);
     throw error;
   }
 };
@@ -15,11 +15,11 @@ export const fetchCategories = async () => {
 export const fetchQuizQuestions = async (amount, category, difficulty) => {
   try {
     const response = await axios.get(API_URL, {
-      params: { amount, category, difficulty, type: 'multiple' }
+      params: { amount, category, difficulty, type: "multiple" },
     });
     return response.data.results;
   } catch (error) {
-    console.error('Error fetching questions:', error);
+    console.error("Error fetching questions:", error);
     throw error;
   }
 };
